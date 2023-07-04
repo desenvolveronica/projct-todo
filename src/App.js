a0be93907498187935c888dcddd3c4dd071e1b35
Verônica Helena <desenvolveronica@gmail.com>
2023-07-04 17:39:20 -0300

    App.js e App.css

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ const API ="http://localhost:5000";
 
 function App() {
   const [title, setTitle] = useState("");
-
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
   return (
     <div className="App">
-      <h1>ToDo</h1>
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      <div className="form-todo">
+        <p>Formulário</p>
+      </div>
+      <div className="list-todo">
+        <p>Lista</p>
+      </div>
+
     </div>
   );
 }
