f556234cbc5a13aef7c5351bbbb98da22465f777
Verônica Helena <desenvolveronica@gmail.com>
2023-07-04 08:53:21 -0300

    complemento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import {useState, useEffect} from 'react'
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs'
+
+const API ="http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("");
+
   return (
     <div className="App">
       <h1>ToDo</h1>
