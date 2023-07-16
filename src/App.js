796daf08be87b68bf9642fd8340a58619031cb1c
Verônica Helena <desenvolveronica@gmail.com>
2023-07-16 15:27:56 -0300

    Objeto para armazenar tarefa

--- src/App.js
+++ src/App.js
@@ -12,8 +12,20 @@ function App() {
   
 const handleSubmit = (e) => {
   e.preventDefault();
+
+  const todo = {
+    id: Math.random(),
+    title,
+    time,
+    done: false,
+  };
+
+  //Envio para API
+  console.log(todo)
+
+  //Zerando os inputs
   setTitle("");
-  console.log(title)
+  setTime("");
 }
   
   return (
@@ -25,7 +37,7 @@ const handleSubmit = (e) => {
         <h2>Insira sua próxima tarefa:</h2>
         <form onSubmit={handleSubmit}>
           <div className='form-control'>
-            <label htmlFor="title">O que você vai fazer?</label>
+            <label htmlFor="title">O que você vai fazer? </label>
             <input type="text" 
             name="title" 
             placeholder='Título da Tarefa' 
@@ -36,6 +48,22 @@ const handleSubmit = (e) => {
 
           </div>
         <input type="submit" value="Enviar"/>
+        </form>
+      </div>
+      <div className="form-todo">
+        <h2>Insira sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor="time">Duração: </label>
+            <input type="text" 
+            name="time" 
+            placeholder='Tempo Estimado (em horas)' 
+            onChange = {(e) => setTime(e.target.value)}
+            value={time || ""}
+            required
+            />
+          </div>
+        <input type="submit" value="Criar Tarefa"/>
         </form>
       </div>
       <div className="list-todo">
